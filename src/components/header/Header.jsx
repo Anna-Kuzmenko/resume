88b81ebdb480f93338be314cf384f9ae7eaa5629
Anna Kuzmenko <mogilevs.anna@gmail.com>
2024-12-09 23:44:06 +0200

    start project

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div className="fixed top-0 left-0 w-full flex items-center h-[100px] bg-[#f8f8f8] px-[32px]">
+      <p className="text-3xl font-bold">
+        Anna Kuzmenko <span className="font-normal">/ Frontend Developer</span>
+      </p>
+    </div>
+  );
+};
+
+export default Header;
